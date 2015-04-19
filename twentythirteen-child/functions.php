230c4107b8e285fd90edd5fddee2af3f6d46d523
Tracy Rotton <tracy@taupecat.com>
2015-04-18 23:10:20 -0400

    following the codex's guide on enqueuing stylesheets for child themes

--- twentythirteen-child/functions.php
+++ twentythirteen-child/functions.php
@@ -1,17 +1,10 @@
 <?php
 
 function ga_stylesheets() {
-	wp_dequeue_style( 'twentythirteen-style' );
-	wp_deregister_style( 'twentythirteen-style' );
-	wp_dequeue_style( 'twentythirteen-fonts' );
-	wp_deregister_style( 'twentythirteen-fonts' );
-
-	wp_register_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-	wp_register_style( 'child-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic|Roboto+Slab:400,700' );
-	wp_register_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style', 'child-fonts' ) );
-	wp_enqueue_style( 'child-style' );
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
 }
-add_action( 'wp_enqueue_scripts', 'ga_stylesheets', 15 );
+add_action( 'wp_enqueue_scripts', 'ga_stylesheets' );
 
 function ga_header_background_positioning() {
 ?>
