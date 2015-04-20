a3f5d701e558c6a0ecf129c4664018073e0b04b2
Tracy Rotton <tracy@taupecat.com>
2015-04-20 16:01:30 -0400

    commented out stuff we're going to work on in class

--- twentythirteen-child/functions.php
+++ twentythirteen-child/functions.php
@@ -1,11 +1,12 @@
 <?php
 
-function ga_stylesheets() {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
-}
-add_action( 'wp_enqueue_scripts', 'ga_stylesheets' );
+// function ga_stylesheets() {
+// 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+// 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
+// }
+// add_action( 'wp_enqueue_scripts', 'ga_stylesheets' );
 
+/*
 function ga_header_background_positioning() {
 ?>
 <style>
@@ -16,8 +17,4 @@ function ga_header_background_positioning() {
 <?php
 }
 add_action( 'wp_head', 'ga_header_background_positioning', 15 );
-
-/**
- * Add support for a custom header image.
- */
-require get_stylesheet_directory() . '/inc/custom-header.php';
+*/
